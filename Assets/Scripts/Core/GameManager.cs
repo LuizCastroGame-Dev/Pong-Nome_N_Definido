49ac18711fcec413082e05d4925e8eaaeef86b58
Luiz Fernando Castro de Araujo <luizfernandocampeao1999@gmail.com>
2026-08-10 15:44:58 -0300

    feat(gameplay): implement Point system - Point wall (Player & Enemy)

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,12 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    [Header("Paddles")]
     public Transform PaddlePlayer;
     public Transform PaddleEnemy;
 
+    [Header("Ball Movement")]
     public BallMovement ballMovement;
+
+    [Header("Points")]
+    public int playerPoints = 0;
+    public int enemyPoints = 0;
+
+    [Header("Points UI")]
+    public TextMeshProUGUI textPointPlayer;
+    public TextMeshProUGUI textPointEnemy;
 
     void Start()
     {
@@ -15,10 +25,31 @@ public class GameManager : MonoBehaviour
 
     private void Resetgame()
     {
+        //Paddle Reset
         PaddlePlayer.position = new Vector3(-7.5f, 0f, 0f);
         PaddleEnemy.position = new Vector3(7.5f, 0f, 0f);
 
+        //Ball Reset
         ballMovement.ResetBall();
+
+        //Points Reset
+        playerPoints = 0;
+        enemyPoints = 0;
+
+        textPointPlayer.text = playerPoints.ToString();
+        textPointEnemy.text = enemyPoints.ToString();
+    }
+
+    public void PlayerScore()
+    {
+        playerPoints++;
+        textPointPlayer.text = playerPoints.ToString();
+    }
+
+    public void EnemyScore()
+    {
+        enemyPoints++;
+        textPointEnemy.text = enemyPoints.ToString();
     }
 
 }
--- Assets/Scripts/Gameplay/Ball/BallMovement.cs
+++ Assets/Scripts/Gameplay/Ball/BallMovement.cs
@@ -5,6 +5,8 @@ public class BallMovement : MonoBehaviour
 
     public BallData ballData;
 
+    public GameManager gameManager;
+
     private Rigidbody2D rb;
 
     public void ResetBall()
@@ -17,6 +19,7 @@ public class BallMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //Se colidir com a parede, ele inverte o sentido Y da bola
         if (collision.gameObject.CompareTag("Wall"))
         {
             Vector2 newVelocity = rb.linearVelocity;
@@ -25,9 +28,23 @@ public class BallMovement : MonoBehaviour
             rb.linearVelocity = newVelocity;
         }
 
+        //Se colidir com os Paddle, ele inverte o sentido X da bola
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
         {
             rb.linearVelocity = new Vector2(-rb.linearVelocity.x, rb.linearVelocity.y);
+        }
+
+        //Se colidir com a parede de pontuacao, ele soma 1 ponto
+        if (collision.gameObject.CompareTag("Wall Player"))
+        {
+            gameManager.EnemyScore();
+            ResetBall();
+        }
+
+        if (collision.gameObject.CompareTag("Wall Enemy"))
+        {
+            gameManager.PlayerScore();
+            ResetBall();
         }
     }
 
