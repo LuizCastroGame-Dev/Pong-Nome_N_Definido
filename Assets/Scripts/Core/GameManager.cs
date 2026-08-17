bb4b6a2382de5cbd3602e3fc143c52f56f8e71df
Luiz Fernando Castro de Araujo <luizfernandocampeao1999@gmail.com>
2026-08-17 11:29:45 -0300

    Add the config for menu - add end game in main scene and win conditions

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,18 +14,33 @@ public class GameManager : MonoBehaviour
     [Header("Points")]
     public int playerPoints = 0;
     public int enemyPoints = 0;
+    public int winPoints = 5;
 
     [Header("Points UI")]
     public TextMeshProUGUI textPointPlayer;
     public TextMeshProUGUI textPointEnemy;
 
+    [Header("End Screen and Interact Objects")]
+    public GameObject screenEndGame;
+    public GameObject interactObjects;
+
     void Start()
     {
+        //Reset the objects positions
         Resetgame();
+    }
+
+    private void Update()
+    {
+        //Checking win condition
+        CheckWin();
     }
 
     private void Resetgame()
     {
+        //interact Objects Reset
+        interactObjects.SetActive(true);
+
         //Paddle Reset
         PaddlePlayer.position = new Vector3(-7.5f, 0f, 0f);
         PaddleEnemy.position = new Vector3(7.5f, 0f, 0f);
@@ -42,14 +58,38 @@ public class GameManager : MonoBehaviour
 
     public void PlayerScore()
     {
+        //Sum playerPoints
         playerPoints++;
         textPointPlayer.text = playerPoints.ToString();
     }
 
     public void EnemyScore()
     {
+        //Sum enemyPoints
         enemyPoints++;
         textPointEnemy.text = enemyPoints.ToString();
     }
 
+    public void CheckWin()
+    {
+        //Win conditions
+        if (playerPoints >= winPoints || enemyPoints >= winPoints) 
+        {
+            EndGame();
+        }
+    }
+
+    public void EndGame()
+    {
+        //Call end game
+        interactObjects.SetActive(false);
+        screenEndGame.SetActive(true);
+        Invoke(nameof(LoadMenu), 2f);
+    }
+
+    public void LoadMenu()
+    {
+        //Call the Menu scene
+        SceneManager.LoadScene("Menu");
+    }
 }
