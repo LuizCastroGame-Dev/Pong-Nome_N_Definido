32f6be0f84b429f03ddaea72a77ce75462d3fc20
Luiz Fernando Castro de Araujo <luizfernandocampeao1999@gmail.com>
2026-09-11 11:56:05 -0300

    update input player

--- Assets/Scripts/Gameplay/Player/PaddleInput.cs
+++ Assets/Scripts/Gameplay/Player/PaddleInput.cs
@@ -10,7 +10,25 @@ public class PaddleInput : MonoBehaviour
 
     public bool InputParry() 
     {
-        bool parryInput = Input.GetButtonDown("Fire1");
+        bool parryInput = Input.GetKeyDown(KeyCode.Space);
         return parryInput;
+    }
+
+    public bool InputSelectLeft()
+    {
+        bool leftInput = Input.GetKeyDown(KeyCode.A);
+        return leftInput;
+    }
+
+    public bool InputSelectRight()
+    {
+        bool rightInput = Input.GetKeyDown(KeyCode.D);
+        return rightInput;
+    }
+
+    public bool InputUsedPowerUp()
+    {
+        bool usedInput = Input.GetKeyDown(KeyCode.E);
+        return usedInput;
     }
 }
